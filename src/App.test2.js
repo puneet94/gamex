efbdd4835e43f91f47eedc89272009d8138f5a13
Puneet Mahendra <puneet.mahendra27@gmail.com>
2021-01-17 21:50:11 +1100

    readme and comments

--- src/App.test2.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Find & track the best free-to-play games! Search for what to play next!");
-  expect(linkElement).toBeInTheDocument();
-});
